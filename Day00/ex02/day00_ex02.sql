623e5dfb885ffd5b28e46f6e5d0c8af6c363d22c
Meabuck Alton <malton@pr-p5.kzn.21-school.ru>
2022-11-29 21:13:24 +0300

    Day00 done

---
+++ Day00/ex02/day00_ex02.sql
@@ -0,0 +1,4 @@
+-- 1)
+SELECT name, rating FROM pizzeria WHERE rating >= 3.5 AND rating <= 5;
+-- 2)
+SELECT name, rating FROM pizzeria WHERE rating BETWEEN 3.5 AND 5;
---
+++ Day00/ex06/day00_ex06.sql
@@ -0,0 +1,12 @@
+SELECT
+    CASE
+        WHEN NAME = 'Denis' THEN 'true'
+        ELSE 'false'
+    END AS check_name
+FROM
+(SELECT
+    (SELECT name FROM person WHERE id = person_order.person_id) AS NAME
+FROM
+    person_order
+WHERE
+    (menu_id BETWEEN 13 AND 14 OR menu_id = 18) AND order_date = '2022-01-07') AS NAME ;
